650613d3cd25432381db177d873a5a11da0a48e8
Lisa <lisawang0498@gmail.com>
2023-12-06 06:21:00 -0500

    scrape info data

--- data/1_data_cleaning.py
+++ data/1_data_cleaning.py
@@ -1,8 +1,11 @@
+import os
+
+
 def compare_event_id():
     # After scraping the events with different types. Compare the event ids from the two files and see if there are any duplicates.
-    with open("workshop_event_id.txt", "r") as f:
+    with open("0_event_id/panel_event_id.txt", "r") as f:
         ids = [line.strip() for line in f]
-    with open("lecture_event_id.txt", "r") as f:
+    with open("0_event_id/workshop_event_id.txt", "r") as f:
         ids_1 = [line.strip() for line in f]
     print(len(set(ids)))
     print(len(set(ids_1)))
@@ -27,6 +30,7 @@ def remove_duplicates_and_summary(input_file, output_file, main_url):
                 line not in unique_lines
                 and not line.startswith("Summary")
                 and not line.startswith("[eventId]")
+                and not line.startswith("https")
             ):
                 url = main_url + line
                 f_out.write(url + "\n")
@@ -35,7 +39,17 @@ def remove_duplicates_and_summary(input_file, output_file, main_url):
 
 
 main_url = "https://dukegroups.com/events/rsvp?id="
-input_file = "student_events_id_0.txt"
-output_file = "student_events_id_0_filtered.txt"
 
-remove_duplicates_and_summary(input_file, output_file, main_url)
+folder_path = "0_event_id"
+output_folder_path = "1_event_url"
+
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    if os.path.isfile(file_path):
+        input_file = file_path
+        output_file_name = filename.replace("_id.txt", "_url.txt")
+        os.path.join(folder_path, filename)
+        output_file = os.path.join(output_folder_path, output_file_name)
+        # Perform operations on the file here
+        print(file_path)
+        remove_duplicates_and_summary(input_file, output_file, main_url)
--- data/2_event_info_scraper.py
+++ data/2_event_info_scraper.py
@@ -1,78 +1,72 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import re
+import os
 
 
-def scrape_event_info(url_file):
+def scrape_event_info(url_file, type_name, type_id, output_file):
     event_info = []
     with open(url_file, "r") as f:
         urls = [line.strip() for line in f]
-    i = 0
-    while i < 50:
-        for url in urls:
-            i = i + 1
-            response = requests.get(url)
-            soup = BeautifulSoup(response.content, "html.parser")
-            name = soup.find("h1", {"class": "rsvp__event-name"})
-            if name is not None and name.text:
-                # Extracting event name
-                name = name.text.strip()
 
-                # Extracting event details
-                event_details_element = soup.find("div", {"id": "event_details"})
-                elements_to_exclude = event_details_element.find_all(["h2", "a"])
-                for element in elements_to_exclude:
-                    element.decompose()
-                event_details_text = event_details_element.get_text(strip=True)
-                # event_tags = [
-                #     span.text.strip()
-                #     for span in soup.select("span.label.label-default.label-tag > span")
-                # ]
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        name = soup.find("h1", {"class": "rsvp__event-name"})
+        if name is not None and name.text:
+            # Extracting event name
+            name = name.text.strip()
 
-                # event_tags = [
-                #     span.text.strip()
-                #     for span in soup.find_all(
-                #         "span", {"class": "label label-default label-tag"}
-                #     )
-                # ]
-                # event_tags = [
-                #     span.find("span").text.strip()
-                #     for span in soup.find_all(
-                #         "span", {"class": "label label-default label-tag"}
-                #     )
-                # ]
-                event_org = soup.find("p", {"class": "rsvp__event-org"}).find("a").text
-                # registered = soup.find("span", {"class": "number"}).text
+            # Extracting event details
+            event_details_element = soup.find("div", {"id": "event_details"})
+            elements_to_exclude = event_details_element.find_all(["h2", "a"])
+            for element in elements_to_exclude:
+                element.decompose()
+            event_details_text = event_details_element.get_text(strip=True)
 
-                # target_div = soup.find("div", id="event_main_card")
+            # Extracting event host
+            event_org = soup.find("p", {"class": "rsvp__event-org"}).find("a").text
 
-                # Extracting location
-                location_text = soup.select_one(".col-md-5 p").text.strip()
+            # Extracting location
+            location_text = soup.select_one(".col-md-5 p").text.strip()
 
-                # Extracting date, time, and timezone
-                target_div = soup.find("div", class_="col-md-4_5")
-                date_text = target_div.find("p", style="margin:0;").text.strip()
-                time_text = (
-                    target_div.find("p", style="margin:0;").find_next("p").text.strip()
-                )
-                timezone_text = target_div.find("span", id="timezone").text.strip()
+            # Extracting date, time, and timezone
+            target_div = soup.find("div", class_="col-md-4_5")
+            date_text = target_div.find("p", style="margin:0;").text.strip()
+            time_text = (
+                target_div.find("p", style="margin:0;").find_next("p").text.strip()
+            )
+            timezone_text = target_div.find("span", id="timezone").text.strip()
 
-                print(location_text)
-                print(time_text)
-                print(timezone_text)
+            event_info.append(
+                {
+                    "title": name,
+                    "details": event_details_text,
+                    "type": type_name,
+                    "type_id": type_id,
+                    "host": event_org,
+                    "date": date_text,
+                    "time": time_text,
+                    "location": location_text,
+                    "link": url,
+                }
+            )
 
-                event_info.append(
-                    {
-                        "name": name,
-                        "details": event_details_text,
-                        "location": location_text,
-                        "time": time_text,
-                    }
-                )
-
-    with open("event_info.csv", "w", neline="") as f:
-        writer = csv.DictWriter(f, fieldnames=["name", "details"])
+    with open(output_file, "w", newline="") as f:
+        writer = csv.DictWriter(
+            f,
+            fieldnames=[
+                "title",
+                "details",
+                "type",
+                "type_id",
+                "host",
+                "date",
+                "time",
+                "location",
+                "link",
+            ],
+        )
         writer.writeheader()
         writer.writerows(event_info)
 
@@ -80,12 +74,52 @@ def scrape_event_info(url_file):
 
 
 def main():
-    output_file = "student_events_id_0_filtered.txt"
-    event_info = scrape_event_info(output_file)
-    # event_info_str = "\n".join(event_info)
-    # with open(input_file, "w") as f:
-    #     f.write(event_info_str)
+    folder_path = "1_event_url"
+    output_folder_path = "2_event_info"
+
+    filenames = sorted(os.listdir(folder_path))
+    output_filenames = [
+        filename.replace("_url.txt", "_info.csv") for filename in filenames
+    ]
+    url_file = [os.path.join(folder_path, filename) for filename in filenames]
+    output_file = [
+        os.path.join(output_folder_path, output_filename)
+        for output_filename in output_filenames
+    ]
+    event_types = [
+        "Health/Wellness",
+        "Social",
+        "Workshop/Short Course",
+        "Lecture/Talk",
+        "Panel/Seminar/Colloquim",
+    ]
+    for i in range(len(filenames)):
+        scrape_event_info(url_file[i], event_types[i], i + 1, output_file[i])
 
 
 if __name__ == "__main__":
     main()
+
+
+# # Extracting event tags?
+# event_tags = [
+#     span.text.strip()
+#     for span in soup.select("span.label.label-default.label-tag > span")
+# ]
+
+# event_tags = [
+#     span.text.strip()
+#     for span in soup.find_all(
+#         "span", {"class": "label label-default label-tag"}
+#     )
+# ]
+# event_tags = [
+#     span.find("span").text.strip()
+#     for span in soup.find_all(
+#         "span", {"class": "label label-default label-tag"}
+#     )
+# ]
+
+# registered = soup.find("span", {"class": "number"}).text
+
+# target_div = soup.find("div", id="event_main_card")
