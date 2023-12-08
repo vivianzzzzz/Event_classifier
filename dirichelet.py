d563188b98c1dc0570440e9843f3f07b65477117
GLI81 <liguangzheng998@hotmail.com>
2023-12-08 16:34:01 -0500

    start dirichelet setup

---
+++ dirichelet.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from scipy.stats import dirichelet
+from sklearn.model_selection import train_test_split
+
+
+class DiricheletClassifier:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
