20a5222e82418b8bf4d9a5101ce648011df1e6a4
GLi81 <90353988+gli81@users.noreply.github.com>
2023-12-10 06:03:00 +0000

    model, synthetic data, model

--- synthetic.py
+++ synthetic.py
@@ -4,6 +4,7 @@ import random
 # Load the CSV file
 df = pd.read_csv('events.csv')
 
+
 # Function to shuffle words in a string
 def shuffle_words(string):
     words = string.split()
@@ -12,6 +13,7 @@ def shuffle_words(string):
 
 # Apply the function to the 'title_details' column
 df['title_details'] = df['title_details'].apply(shuffle_words)
+print(df.head())
 
 df.to_csv('events_shuffled.csv', index=False)
 
